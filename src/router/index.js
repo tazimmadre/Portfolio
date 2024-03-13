0bc1afa5d2d6d459962ef37edea8f86ae97cf8a1
developer1-saharsh-software <tazim.madre@saharshsoftware.com>
2024-03-13 13:44:38 +0530

    Routing added

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import NotFound from '../components/Notfound';
 import Home from '../pages/home';
 import BlogPage from '../pages/blog';
 
 const AppRouter = () => {
   return (
-    <Router>
+    <>
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route path="/blog" element={<BlogPage/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
-    </Router>
+      </>
   );
 };
 
